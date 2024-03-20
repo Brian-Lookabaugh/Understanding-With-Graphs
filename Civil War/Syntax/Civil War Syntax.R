3072524932276a52b44537f07e303a92b07e9f12
Brian-Lookabaugh <brianlookabaugh21@gmail.com>
2024-03-20 09:14:27 -0500

    Updating the syntax

--- Civil War/Syntax/Civil War Syntax.R
+++ Civil War/Syntax/Civil War Syntax.R
@@ -4,6 +4,7 @@
 
 pacman::p_load(
   "dplyr", # Data Manipulation
+  "stringr", # Working with Texts
   "ggplot2", # Visualization
   "peacesciencer", # Conflict Data
   "viridis", # Pretty Color Palettes
@@ -25,7 +26,7 @@ term <- read_excel("Civil War/Data/ucdp-term-acd-3-2021 (4).xlsx")
 ucdp <- ucdp %>%
   mutate(gwno.a = as.numeric(gwno_a)) %>%
   # Only Keep Civil Wars
-  filter(type_of_conflict == 3) %>%
+  filter(type_of_conflict == 3 | type_of_conflict == 4) %>%
   # Create a Civil War Dummy Variable
   mutate(civ.war = 1) %>%
   # Collapse This Data to the Country-Year Level
@@ -40,10 +41,10 @@ states <- create_stateyears(system = "gw")
 conflict <- left_join(states, ucdp, by = c("gwcode" = "gwno.a", "year"))
 
 conflict <- conflict %>%
-  # Filter Civil War Years Outside of Time Frame
-  filter(year > 1949 & year < 2021) %>%
   # Re-Code NA Civil War Values to 0
   mutate(civ.war = ifelse(is.na(civ.war), 0, civ.war)) %>%
+  # Keep Relevant Years
+  filter(year >= 1950 & year < 2021) %>%
   # Add Rugged Terrain Data from the {peacesciencer} Package
   add_rugged_terrain()
 
@@ -69,10 +70,8 @@ merged <- left_join(conflict, vdem.filtered, by = c("gwcode" = "COWcode", "year"
 merged <- merged %>%
   # Re-Code Coup Attempts as Non-Civil War Cases
   mutate(civ.war = ifelse(e_pt_coup_attempts > 0, 0, civ.war)) %>%
-  # Re-Code Observations Where Civil War = 0 For One Year to Civil War
-  group_by(gwcode) %>%
-  mutate(civ.war = ifelse(lag(civ.war) == 1 & lead(civ.war == 1), 1, civ.war)) %>%
   # Create an "Ever Civil War" Variable
+  group_by(gwcode) %>%
   arrange(gwcode, year) %>%
   mutate(
     first.civ.war.year = min(year[civ.war == 1]), 
@@ -136,22 +135,90 @@ merged <- left_join(merged, term, by = c("gwcode", "year"))
 # Clean Final Data Set
 final <- merged %>%
   # Filter Variables
-  select(-c("newlmtnest", "country_name"))
+  select(-c("newlmtnest", "country_name")) %>%
+  # Re-Code US Values to Civil War = 0
+  mutate(civ.war = ifelse(gwcode == 2, 0, civ.war))
 
 # Remove Data Sets
 rm(conflict, ged, merged, states, term, ucdp, vdem, vdem.filtered)
 
-################################################################################
-# Graph 1: Time Series Plot of Number of Conflicts Each Year by Conflict Type ##
-################################################################################
+# Create a Custom Graphics Theme
+theme.forester <- function() {
+  theme_minimal() +
+    theme(panel.grid.minor = element_blank(),
+          plot.background = element_rect(fill = "white", color = NA),
+          plot.title = element_text(face = "bold"),
+          axis.title = element_text(face = "bold"),
+          strip.text = element_text(face = "bold", size = rel(0.8), hjust = 0),
+          strip.background = element_rect(fill = "grey80", color = NA),
+          legend.title = element_text(face = "bold"))
+}
 
 ################################################################################
-## Graph 2: Map of Countries Who Experienced/Currently Experiencing Civil War ##
+######### Graph 1: Time Series Plot of Number of Conflicts Each Year ###########
+################################################################################
+
+final %>%
+  # Filter Non-Civil War Cases
+  filter(civ.war == 1) %>%
+  # Create Counts for Civil Wars
+  group_by(year) %>%
+  mutate(civ.war.count = sum(civ.war == 1)) %>%
+  ungroup() %>%
+  # Create the Plot
+  ggplot(aes(x = year)) +
+  geom_line(aes(y = civ.war.count), size = 1, color = "#1A4314") +
+  geom_smooth(aes(y = civ.war.count), method = "loess", se = FALSE, 
+              size = 1, color = "#B8D8AAFF", linetype = "dashed") +
+  scale_x_continuous(breaks = seq(1950, 2020, by = 5)) +
+  labs(
+    title = "Number of Countries with Active Civil Conflicts (1950-2020)",
+    x = "",
+    y = ""
+  ) +
+  theme.forester()
+
+################################################################################
+########## Graph 2: Map of Countries Who Have Had The Most Civil Wars ##########
 ################################################################################
 
 ################################################################################
 ############## Graph 3: Bar Chart of Duration of Current Conflicts #############
 ################################################################################
+
+final %>%
+  # Only Keep Countries With Civil Wars By 2020
+  filter(!is.na(civ.war)) %>%
+  group_by(gwcode) %>%
+  mutate(peace.fail = ifelse(row_number() == 1 & civ.war == 1, 1,
+                             ifelse(lag(civ.war == 0) & civ.war == 1, 1, 0))) %>%
+  ungroup() %>%
+  mutate(id = cumsum(peace.fail)) %>%
+  filter(civ.war == 1) %>%
+  group_by(id) %>%
+  mutate(duration = row_number()) %>%
+  ungroup() %>%
+  mutate(statename = ifelse(statename == "Russia (Soviet Union)", "Russia", statename),
+         statename = ifelse(statename == "Burkina Faso (Upper Volta)", "Burkina Faso", statename),
+         statename = ifelse(statename == "Congo, Democratic Republic of (Zaire)", "DRC", statename),
+         statename = ifelse(statename == "Tanzania/Tanganyika", "Tanzania", statename),
+         statename = ifelse(statename == "Turkey (Ottoman Empire)", "Turkey", statename),
+         statename = ifelse(statename == "Myanmar (Burma)", "Myanmar", statename),
+         statename = ifelse(statename == "Iran (Persia)", "Iran", statename),
+         statename = ifelse(statename == "Central African Republic", "CAR", statename)) %>%
+  # Create the Plot
+  filter(year == 2020) %>%
+  ggplot(aes(x = duration, y = reorder(factor(statename), duration), fill = duration)) +
+  geom_col() +
+  scale_x_continuous(breaks = seq(0, 50, by = 5)) +
+  labs(
+    title = "Duration of Ongoing Civil Conflicts Up to 2020",
+    x = "Years of Continuous Conflict",
+    y = ""
+  ) +
+  scale_fill_viridis(option = "mako", end = 0.8) +
+  theme.forester() +
+  theme(legend.position = "none")
 
 ################################################################################
 ############ Graph 4: Map of Deaths per capita for Current Conflicts ###########
